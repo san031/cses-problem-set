c7c25e5f33da0fb4f0f932674c9af5572b6a1bff
san031 <98796563+san031@users.noreply.github.com>
2024-03-08 00:41:00 +0530

    Add files via upload

---
+++ weird_algorithm.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+int main(){
+    long long n;
+    cin>>n;
+    cout<<n<<" ";
+    while(n!=1){
+        if(n % 2){
+            n=n*3+1;
+        }
+        else{
+            n/=2;
+        }cout<<n<<" ";
+    }return 0;
+}
